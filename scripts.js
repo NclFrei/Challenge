9413c4f6d70a3defd0ccf0174fc4d21218748f17
Nicollas Frei <nicollasfreieleoterio@gmail.com>
2024-05-24 15:57:22 -0300

    Arrumando o carrossel e alguns detalhes

---
+++ scripts.js
@@ -0,0 +1,47 @@
+const servicosSwiper = new Swiper('.servicos__cards', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+        },
+    },
+});
+
+const beneficiosSwiper = new Swiper('.beneficios__cards', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    },
+});
